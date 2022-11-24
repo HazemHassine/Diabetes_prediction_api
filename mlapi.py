9dc67498ef5d7bbc6ecc1d7a934a35c7f681b682
hazem hassine <70240216+HazemHassine@users.noreply.github.com>
2022-11-24 18:46:00 +0100

    everything working locally

--- mlapi.py
+++ mlapi.py
@@ -6,6 +6,7 @@ app = FastAPI()
 
 class request(BaseModel):
 	pregnancies: int
+	bloodpressure: int
 	glucose: int
 	skinThickness: int
 	insulin: int
@@ -17,18 +18,19 @@ class request(BaseModel):
 with open("model_pkl.pkl", "rb") as f:
 	model = pkl.load(f)
 
-@app.get('/'):
+@app.get('/')
 def index():
-	return "<h1>This is the diabetes api</h1>"
+	return "This is the diabetes api The request should be in json form containg these fields:pregnancies: int/bloodpressure: int/glucose: in/skinThickness: int/insulin: int/bmi: float/diabetesPedigreeFunction: int/age: int"
 
 @app.post('/predict/')
 def get_prediction(req: request):
 	data = req.dict()
-	pregnancies = req["pregnancies"] 
-	glucose = req["glucose"]
-	skinThickness = req["skinThickness"]
-	insulin = req["insulin"]
-	bmi = req["bmi"]
-	diabetesPedigreeFunction = req["diabetesPedigreeFunction"]
-	age = req["age"]
-	return {'prediction': model.predict([[pregnancies,glucose,skinThickness,insulin,bmi,diabetesPedigreeFunction,age]])}
+	pregnancies = data["pregnancies"] 
+	glucose = data["glucose"]
+	bloodpressure = data["bloodpressure"]
+	skinThickness = data["skinThickness"]
+	insulin = data["insulin"]
+	bmi = data["bmi"]
+	diabetesPedigreeFunction = data["diabetesPedigreeFunction"]
+	age = data["age"]
+	return {'prediction': str(model.predict([[pregnancies,glucose,bloodpressure,skinThickness,insulin,bmi,diabetesPedigreeFunction,age]])[0])}
